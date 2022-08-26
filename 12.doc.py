e56a1db6b3b3b34b9facf44e4a293aae7911568f
POOJABINJWA <110899989+POOJABINJWA@users.noreply.github.com>
2022-08-26 11:37:17 +0530

    Update 12.doc.py

--- 12.doc.py
+++ 12.doc.py
@@ -1,29 +1,29 @@
-# Q12.Numbers ending with zeros are boring.
+Q12.Numbers ending with zeros are boring.
 
-# They might be fun in your world, but not here.
-# Get rid of them. Only the ending ones.
-# 1450 -> 145
-# 960000 -> 96
-# 1050 -> 105
-# -1050 -> -105
+They might be fun in your world, but not here.
+Get rid of them. Only the ending ones.
+1450 -> 145
+960000 -> 96
+1050 -> 105
+-1050 -> -105
 
-# def fun():
-#     a=[1450,96000,1050,-1050]
-#     d=str(a)
-#     i=0
-#     b=[]
-#     while i<len(a):
-#         if type(d[i])==list:
-#            j=0
-#            while j<len(a[i]):
-#                 if d[i][j]!="0":
-#                     b.append(a[i][j])
-#                 j=j+1
-#         else:
-#             b.append(a[i])        
-#         i=i+1
-#     print(b)
-# fun()    
+def fun():
+    a=[1450,96000,1050,-1050]
+    d=str(a)
+    i=0
+    b=[]
+    while i<len(a):
+        if type(d[i])==list:
+           j=0
+           while j<len(a[i]):
+                if d[i][j]!="0":
+                    b.append(a[i][j])
+                j=j+1
+        else:
+            b.append(a[i])        
+        i=i+1
+    print(b)
+fun()    
 
 
 
@@ -43,20 +43,20 @@ def fun(a):
 fun([1450,96000,1050,-1050])
 
 
-# def fun():
-#     a=[1450,96000,1050,-1050]
-#     i=0
-#     b=[]
-#     d=str(a)
-#     while i<len(a):
-#         while a[i]>0:
-#             if a[i]%10!=0:
-#                 b.append(a[i])
-#                 break
-#             a[i]//=10
-#         i=i+1
-#     print(b)
-# fun()
+def fun():
+    a=[1450,96000,1050,-1050]
+    i=0
+    b=[]
+    d=str(a)
+    while i<len(a):
+        while a[i]>0:
+            if a[i]%10!=0:
+                b.append(a[i])
+                break
+            a[i]//=10
+        i=i+1
+    print(b)
+fun()
 
 
 
